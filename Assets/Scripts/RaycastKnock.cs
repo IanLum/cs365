a2940abe8a8539106914d9062a4af13ce8baadc3
zozoeyey <zy103@wellesley.edu>
2025-03-03 20:00:55 -0500

    added raycasting

--- Assets/Scripts/RaycastKnock.cs
+++ Assets/Scripts/RaycastKnock.cs
@@ -4,28 +4,38 @@ public class RaycastKnock : MonoBehaviour
 {
     public MorseSequence morseSequence;
     public GameObject instructionPanel;
+    private Camera cam;
+
+
+    void Start()
+    {
+       cam = GetComponent<Camera>();
+    }
 
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position,transform.forward, out hit, 6))
+        //cam.tranform
+        if (Physics.Raycast(transform.position, cam.transform.forward, out hit, 6))
         {
-
-            var selection = hit.transform;
-            if (selection.CompareTag("Obstacle"))
+            if (hit.distance > 1.5)
             {
-                morseSequence.canKnock = true;
+                var selection = hit.transform;
+                if (selection.CompareTag("Obstacle"))
+                {
+                    morseSequence.canKnock = true;
+                }
+                else
+                {
+                    morseSequence.canKnock = false;
+                    instructionPanel.SetActive(false);
+                }
             }
             else
             {
                 morseSequence.canKnock = false;
                 instructionPanel.SetActive(false);
             }
-        }
-        else
-        {
-            morseSequence.canKnock = false;
-            instructionPanel.SetActive(false);
         }
     }
 }
