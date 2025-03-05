a16fc494e20893dfbdb4d29d7ab839442be6531f
Ian Lum <ianlum314@gmail.com>
2025-03-05 01:41:48 -0500

    audio indicator ui

--- Assets/Scripts/MorseSequence/AudioSequenceEmitter.cs
+++ Assets/Scripts/MorseSequence/AudioSequenceEmitter.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class AudioSequenceEmitter : MonoBehaviour
 {
     const float DOT_TIME = 0.2f;
     const float DASH_TIME = 0.7f;
     const float TIME_BETWEEN_SEQUENCE = 4f;
+    // AudioIndicatorUI connects to this signal
+    public UnityEvent<AudioSequenceEmitter> AudioEmitted;
     public enum Notes { DOT, DASH };
     public AudioSource audioSource;
     public Notes[] sequence;
@@ -22,6 +25,7 @@ public class AudioSequenceEmitter : MonoBehaviour
             {
                 float waitTime = (note == Notes.DOT) ? DOT_TIME : DASH_TIME;
                 audioSource.Play();
+                AudioEmitted.Invoke(this);
                 yield return new WaitForSeconds(waitTime);
             }
             yield return new WaitForSeconds(TIME_BETWEEN_SEQUENCE);
