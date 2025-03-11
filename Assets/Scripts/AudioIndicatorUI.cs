16ff73191ea94654515fc3a30358db89512c0c28
Ian Lum <ianlum314@gmail.com>
2025-03-11 13:20:15 -0400

    no indicator if too far

--- Assets/Scripts/AudioIndicatorUI.cs
+++ Assets/Scripts/AudioIndicatorUI.cs
@@ -21,8 +21,14 @@ public class AudioIndicatorUI : MonoBehaviour
         // TODO: more juice, slight drift forward? smaller indicator that
         // echoes forward? scale size by distance to sound
 
+        // If outside of max dist, do nothing
+        Vector3 vec_to_player = (emitter.GetComponent<Transform>().position - Player.position);
+        if (vec_to_player.magnitude > emitter.audioSource.maxDistance)
+        {
+            return;
+        }
         // Point indicator towards source of sound
-        Vector3 dir = (emitter.GetComponent<Transform>().position - Player.position).normalized;
+        Vector3 dir = vec_to_player.normalized;
         float angle = Vector3.SignedAngle(dir, Player.forward, Vector3.up);
         Pivot.transform.localEulerAngles = new Vector3(0, 0, angle);
 
