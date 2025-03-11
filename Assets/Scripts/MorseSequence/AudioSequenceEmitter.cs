2fb4f0f2ef2f02251198c53e68e3a9180276d419
Ian Lum <ianlum314@gmail.com>
2025-03-10 22:37:29 -0400

    sync droplets with audio pattern

--- Assets/Scripts/MorseSequence/AudioSequenceEmitter.cs
+++ Assets/Scripts/MorseSequence/AudioSequenceEmitter.cs
@@ -8,9 +8,14 @@ public class AudioSequenceEmitter : MonoBehaviour
     const float DASH_TIME = 0.7f;
     const float TIME_BETWEEN_SEQUENCE = 4f;
     // AudioIndicatorUI connects to this signal
-    public UnityEvent<AudioSequenceEmitter> AudioEmitted;
+    [HideInInspector] public UnityEvent<AudioSequenceEmitter> AudioEmitted;
     public enum Notes { DOT, DASH };
     public AudioSource audioSource;
+    // Optional, expects non looping particle system
+    public ParticleSystem particleSystemObj;
+    // Waits this amount between spawning a particle and playing sound
+    // Causes unexpected behavior if longer than DOT_TIME
+    public float particleDuration;
     public Notes[] sequence;
     void Start()
     {
@@ -24,11 +29,16 @@ public class AudioSequenceEmitter : MonoBehaviour
             foreach (Notes note in sequence)
             {
                 float waitTime = (note == Notes.DOT) ? DOT_TIME : DASH_TIME;
+                if (particleSystemObj != null)
+                {
+                    particleSystemObj.Play();
+                    yield return new WaitForSeconds(particleDuration);
+                }
                 audioSource.Play();
                 AudioEmitted.Invoke(this);
-                yield return new WaitForSeconds(waitTime);
+                yield return new WaitForSeconds(waitTime - particleDuration);
             }
-            yield return new WaitForSeconds(TIME_BETWEEN_SEQUENCE);
+            yield return new WaitForSeconds(TIME_BETWEEN_SEQUENCE - particleDuration);
         }
     }
 }
